643a9b70ae7ccb04141a20b0d44b7018ba0eaf43
Hoomi1 <glebbambam@yandex.ru>
2020-04-16 14:29:58 +0300

    Lab_2

---
+++ ex3.js
@@ -0,0 +1,5 @@
+function min(a,b) {
+if(a < b)
+ document.write(a);
+}
+document.write(min(10,50));
---
+++ pik.js
@@ -0,0 +1,5 @@
+function calcD(a,b,c) {
+
+ return b*b - 4*a*c;
+}
+document.write(calcD(1, 3, 2));
